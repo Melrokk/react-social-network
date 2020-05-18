8cf0ad0e1e13d73391fc57d024291dcdd8b2b480
Melrokk <melrokk@bk.ru>
2020-05-18 20:15:10 +0700

    Add users class component

--- src/component/Navigation/Navigation.jsx
+++ src/component/Navigation/Navigation.jsx
@@ -9,6 +9,7 @@ const Navigation = (props) => {
       <nav className={style.nav}>
           <div><NavLink className={style.item} activeClassName={style.active} to={"/profile"}>Profile</NavLink></div>
           <div><NavLink className={style.item} activeClassName={style.active} to={"/dialogs"}>Massages</NavLink></div>
+          <div><NavLink className={style.item} activeClassName={style.active} to={"/users"}>Users</NavLink></div>
           <div><NavLink className={style.item} activeClassName={style.active} to={"/news"}>News</NavLink></div>
           <div><NavLink className={style.item} activeClassName={style.active} to={"/music"}>Music</NavLink></div>
           <div className={style.settings}><NavLink className={style.item} activeClassName={style.active} to={"/settings"}>Settings</NavLink></div>
--- src/component/Users/Users.jsx
+++ src/component/Users/Users.jsx
@@ -1,57 +1,20 @@
 import React from "react";
 import style from './Users.module.scss';
 import classNames from 'classnames';
+import * as axios from "axios";
+import userPhoto from "../../assets/images/default-user.png"
 
 let Users = (props) => {
 
-    if(props.usersData.length === 0){
-        props.setUsers([
-            {
-                id: 1,
-                fullName: "Bob",
-                status: "im a dog, not a god!",
-                logoUrl: "https://www.kindpng.com/userimgs/710.jpg",
-                followed: false,
-                location: {
-                    city: "New York",
-                    country: "USA"
-                }
-            },
-            {
-                id: 2,
-                fullName: "Lisa",
-                status: "Lets rock!",
-                logoUrl: "https://www.kindpng.com/userimgs/752.jpg",
-                followed: true,
-                location: {
-                    city: "Moscow",
-                    country: "Russian Federation"
-                }
-            },
-            {
-                id: 3,
-                fullName: "Max",
-                status: "No pain, no game!",
-                logoUrl: "https://www.kindpng.com/userimgs/7555.jpg",
-                followed: false,
-                location: {
-                    city: "Kazan",
-                    country: "Russian Federation"
-                }
-            },
-            {
-                id: 4,
-                fullName: "Roberta",
-                status: "Booykasha!",
-                logoUrl: "https://www.kindpng.com/userimgs/760.jpg",
-                followed: true,
-                location: {
-                    city: "Minsk",
-                    country: "Belarus"
-                }
-            },
-        ]);
-    }
+    let getUsers = () => {
+        if (props.usersData.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                console.log('response', response);
+                console.log('response.data.items', response.data.items);
+                props.setUsers(response.data.items);
+            });
+        }
+    };
 
     let buttonSmallPrimary = classNames(
         style.button,
@@ -68,22 +31,27 @@ let Users = (props) => {
     return (
         <div className={style.users_block}>
             <h2>Users List</h2>
+            <button className={buttonSmallPrimary} onClick={getUsers}>Get Users</button>
             {
                 props.usersData.map(user =>
                     <div key={user.id} className={style.user_item}>
                         <div className={style.user_img_block}>
-                            <img src={user.logoUrl}/>
+                            <img src={user.photos.small != null ? user.photos.small : userPhoto}/>
                             {user.followed ?
-                                <button onClick={() => {props.unfollow(user.id)}} className={buttonSmallSoftPrimary}> Unfollow </button> :
-                                <button onClick={() => {props.follow(user.id)}} className={buttonSmallPrimary}> Follow </button>
+                                <button onClick={() => {
+                                    props.unfollow(user.id)
+                                }} className={buttonSmallSoftPrimary}> Unfollow </button> :
+                                <button onClick={() => {
+                                    props.follow(user.id)
+                                }} className={buttonSmallPrimary}> Follow </button>
                             }
 
                         </div>
                         <div className={style.user_data_block}>
-                            <div className={style.user_name}>{user.fullName}</div>
+                            <div className={style.user_name}>{user.name}</div>
                             <div className={style.user_status}>{user.status}</div>
-                            <div className={style.user_country}>{user.location.country}</div>
-                            <div className={style.user_city}>{user.location.city}</div>
+                            <div className={style.user_country}>{"user.location.country"}</div>
+                            <div className={style.user_city}>{"user.location.city"}</div>
                         </div>
                     </div>)
             }
